4e9eb239adda5a6bfa45ed8d422c7b37f63e0cf5
Ivan Gabriele <ivan.gabriele@gmail.com>
2016-07-05 01:45:06 +0200

    Build: Browsersync, Demo generator & builder

--- gruntfile.js
+++ gruntfile.js
@@ -1,51 +1,114 @@
-module.exports = (grunt) => {
-  'use strict';
+'use strict'
+
+module.exports = grunt => {
+  require('load-grunt-tasks')(grunt)
 
   grunt.initConfig({
     babel: {
-        options: {
-            sourceMap: false,
-            presets: ['es2015']
-        },
-        dist: {
-            files: {
-                'dist/bootstrap-without-jquery.js': 'src/bootstrap-without-jquery.js'
-            }
+      dist: {
+        files: {
+          'dist/bootstrap-without-jquery.js': 'src/bootstrap-without-jquery.js'
         }
+      }
+    },
+    browserSync: {
+      bsFiles: {
+        src: 'demo/bootstrap-without-jquery.js'
+      },
+      options: {
+        server: './demo',
+        watchTask: true
+      }
+    },
+    copy: {
+      bootstrapExample: {
+        expand: true,
+        flatten: true,
+        cwd: 'bootstrap/docs/examples/theme',
+        src: '*',
+        dest: 'demo'
+      },
+      bootstrapAssets: {
+        expand: true,
+        // flatten: true,
+        cwd: 'bootstrap/docs',
+        src: [
+          'assets/**',
+          'dist/**'
+        ],
+        dest: 'demo'
+      },
+      distToDemo: {
+        expand: true,
+        cwd: 'dist',
+        src: 'bootstrap-without-jquery.js',
+        dest: 'demo'
+      }
     },
     jshint: {
+      all: [
+        'bower.json',
+        'package.json',
+        'src/bootstrap-without-jquery.js'
+      ],
       options: {
-        browser: true,
-        camelcase: true,
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        noempty: true,
-        quotmark: true,
-        undef: true,
-        unused: true,
-        expr: true
+        jshintrc: '.jshintrc'
+      }
+    },
+    replace: {
+      demo: {
+        options: {
+          patterns: [
+            {
+              match: /\.\.\/\.\.\//g,
+              replacement: ''
+            },
+            {
+              match: /(<\/body>)/g,
+              replacement: '<script src="bootstrap-without-jquery.js"></script>$1'
+            },
+            {
+              match: /<script.*\/script>/g,
+              replacement: ''
+            }
+          ]
+        },
+        files: [{
+          expand: true,
+          flatten: true,
+          src: 'demo/index.html',
+          dest: 'demo'
+        }]
       },
-      all: ['bower.json', 'package.json', 'bootstrap2/bootstrap-without-jquery.js', 'bootstrap3/bootstrap-without-jquery.js']
     },
     uglify: {
+      dist: {
+        files: {
+          'dist/bootstrap-without-jquery.min.js': 'dist/bootstrap-without-jquery.js'
+        }
+      },
       options: {
         preserveComments: false,
         sourceMap: false
       }
-      dist: {
-        files: {
-          'dist/bootstrap-without-jquery.min.js': 'dist/bootstrap-without-jquery.js',
-        }
-      }
+    },
+    watch: {
+      files: 'src/bootstrap-without-jquery.js',
+      tasks: ['babel', 'copy:distToDemo']
     }
   })
 
-  grunt.loadNpmTasks('grunt-contrib-jshint')
-  grunt.loadNpmTasks('grunt-contrib-uglify')
+  grunt.registerTask('build', [
+    'demo',
+    'jshint',
+    'babel',
+    'uglify'
+  ])
 
-  grunt.registerTask('default', ['babel', 'uglify'])
+  grunt.registerTask('demo', [
+    'copy',
+    'replace',
+  ])
+
+  grunt.registerTask('default', ['browserSync', 'watch'])
 }
