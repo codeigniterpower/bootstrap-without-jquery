d25d184bc3e3a6766c65a5b2abe7e4aeb30978a3
Ivan Gabriele <ivan.gabriele@gmail.com>
2017-02-26 23:46:14 +0100

    Dev: Add modal dialogs support

--- demo/bootstrap-without-jquery.js
+++ demo/bootstrap-without-jquery.js
@@ -9,7 +9,8 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
   var Utils = {
     /**
-     * Remove
+     * Remove a DOM element selected via itself or an event (target)
+     *
      * @param  {HTMLElement|Event} nodeOrEvent [description]
      * @return {void}
      */
@@ -47,42 +48,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
     return DismissableAlertComponent;
   }();
 
-  var DropDownMenuComponent = function () {
-    function DropDownMenuComponent(node) {
-      _classCallCheck(this, DropDownMenuComponent);
-
-      node.addEventListener('click', this.open);
-      node.addEventListener('blur', this.close);
-    }
-
-    _createClass(DropDownMenuComponent, [{
-      key: 'open',
-      value: function open(event) {
-        event.preventDefault();
-
-        event.currentTarget.parentElement.classList.toggle('open');
-      }
-    }, {
-      key: 'close',
-      value: function close(event) {
-        event.preventDefault();
-
-        event.currentTarget.parentElement.classList.remove('open');
-
-        // Trigger the click event on the target if it not opening another menu
-        if (event.relatedTarget && event.relatedTarget.getAttribute('data-toggle') !== 'dropdown') {
-          event.relatedTarget.click();
-        }
-      }
-    }]);
-
-    return DropDownMenuComponent;
-  }();
-
   var CollapsableBehavior = function () {
-    // private _originNode
-    // private _targetNode
-
     function CollapsableBehavior(node) {
       var _this = this;
 
@@ -187,33 +153,114 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
     return CollapsableBehavior;
   }();
 
-  var collapsableNodes = document.querySelectorAll('[data-toggle=collapse]');
-  var _iteratorNormalCompletion = true;
-  var _didIteratorError = false;
-  var _iteratorError = undefined;
+  var DropDownMenuComponent = function () {
+    function DropDownMenuComponent(node) {
+      _classCallCheck(this, DropDownMenuComponent);
 
-  try {
-    for (var _iterator = collapsableNodes[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-      var collapsableNode = _step.value;
-
-      new CollapsableBehavior(collapsableNode);
+      node.addEventListener('click', this.open);
+      node.addEventListener('blur', this.close);
     }
-  } catch (err) {
-    _didIteratorError = true;
-    _iteratorError = err;
-  } finally {
-    try {
-      if (!_iteratorNormalCompletion && _iterator.return) {
-        _iterator.return();
-      }
-    } finally {
-      if (_didIteratorError) {
-        throw _iteratorError;
-      }
-    }
-  }
 
-  var alertNodes = document.querySelectorAll('[data-dismiss=alert]');
+    _createClass(DropDownMenuComponent, [{
+      key: 'open',
+      value: function open(event) {
+        event.preventDefault();
+        event.currentTarget.parentElement.classList.toggle('open');
+      }
+    }, {
+      key: 'close',
+      value: function close(event) {
+        event.preventDefault();
+        event.currentTarget.parentElement.classList.remove('open');
+
+        // Trigger the click event on the target if it not opening another menu
+        if (event.relatedTarget && event.relatedTarget.getAttribute('data-toggle') !== 'dropdown') {
+          event.relatedTarget.click();
+        }
+      }
+    }]);
+
+    return DropDownMenuComponent;
+  }();
+
+  var ModalComponent = function () {
+    function ModalComponent(modalOpenButtonNode) {
+      _classCallCheck(this, ModalComponent);
+
+      this._isFirstTime = true;
+      this._bodyNode = document.querySelector('body');
+      this._modalWindowNode = document.querySelector(modalOpenButtonNode.dataset.target);
+
+      modalOpenButtonNode.addEventListener('click', this.open.bind(this));
+    }
+
+    _createClass(ModalComponent, [{
+      key: 'open',
+      value: function open(event) {
+        event.preventDefault();
+
+        // We create and insert the background
+        this._modalBackdropNode = document.createElement('div');
+        this._modalBackdropNode.classList.add('modal-backdrop', 'fade', 'in');
+        this._bodyNode.appendChild(this._modalBackdropNode);
+
+        // We prepare the body
+        this._bodyNode.classList.add('modal-open');
+        this._bodyNode.style.paddingRight = '17px';
+
+        // We show the modal
+        this._modalWindowNode.classList.add('in');
+        this._modalWindowNode.style.display = 'block';
+
+        if (this._isFirstTime) {
+          this._isFirstTime = false;
+
+          // We attach the close event to the dismiss buttons
+          var modalCloseButtonNodes = document.querySelectorAll('[data-dismiss="modal"]');
+          var _iteratorNormalCompletion = true;
+          var _didIteratorError = false;
+          var _iteratorError = undefined;
+
+          try {
+            for (var _iterator = modalCloseButtonNodes[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+              var modalCloseButtonNode = _step.value;
+
+              modalCloseButtonNode.addEventListener('click', this.close.bind(this));
+            }
+          } catch (err) {
+            _didIteratorError = true;
+            _iteratorError = err;
+          } finally {
+            try {
+              if (!_iteratorNormalCompletion && _iterator.return) {
+                _iterator.return();
+              }
+            } finally {
+              if (_didIteratorError) {
+                throw _iteratorError;
+              }
+            }
+          }
+        }
+      }
+    }, {
+      key: 'close',
+      value: function close(event) {
+        event.preventDefault();
+
+        this._bodyNode.classList.remove('modal-open');
+        this._bodyNode.style.paddingRight = null;
+        this._modalWindowNode.classList.remove('in');
+        this._modalWindowNode.style.display = 'none';
+
+        Utils.remove(this._modalBackdropNode);
+      }
+    }]);
+
+    return ModalComponent;
+  }();
+
+  var alertNodes = document.querySelectorAll('[data-dismiss="alert"]');
   var _iteratorNormalCompletion2 = true;
   var _didIteratorError2 = false;
   var _iteratorError2 = undefined;
@@ -239,16 +286,16 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
     }
   }
 
-  var dropDownNodes = document.querySelectorAll('[data-toggle=dropdown]');
+  var collapsableNodes = document.querySelectorAll('[data-toggle="collapse"]');
   var _iteratorNormalCompletion3 = true;
   var _didIteratorError3 = false;
   var _iteratorError3 = undefined;
 
   try {
-    for (var _iterator3 = dropDownNodes[Symbol.iterator](), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
-      var dropDownNode = _step3.value;
+    for (var _iterator3 = collapsableNodes[Symbol.iterator](), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
+      var collapsableNode = _step3.value;
 
-      new DropDownMenuComponent(dropDownNode);
+      new CollapsableBehavior(collapsableNode);
     }
   } catch (err) {
     _didIteratorError3 = true;
@@ -261,6 +308,58 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
     } finally {
       if (_didIteratorError3) {
         throw _iteratorError3;
+      }
+    }
+  }
+
+  var dropDownNodes = document.querySelectorAll('[data-toggle="dropdown"]');
+  var _iteratorNormalCompletion4 = true;
+  var _didIteratorError4 = false;
+  var _iteratorError4 = undefined;
+
+  try {
+    for (var _iterator4 = dropDownNodes[Symbol.iterator](), _step4; !(_iteratorNormalCompletion4 = (_step4 = _iterator4.next()).done); _iteratorNormalCompletion4 = true) {
+      var dropDownNode = _step4.value;
+
+      new DropDownMenuComponent(dropDownNode);
+    }
+  } catch (err) {
+    _didIteratorError4 = true;
+    _iteratorError4 = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion4 && _iterator4.return) {
+        _iterator4.return();
+      }
+    } finally {
+      if (_didIteratorError4) {
+        throw _iteratorError4;
+      }
+    }
+  }
+
+  var modalOpenButtonNodes = document.querySelectorAll('[data-toggle="modal"]');
+  var _iteratorNormalCompletion5 = true;
+  var _didIteratorError5 = false;
+  var _iteratorError5 = undefined;
+
+  try {
+    for (var _iterator5 = modalOpenButtonNodes[Symbol.iterator](), _step5; !(_iteratorNormalCompletion5 = (_step5 = _iterator5.next()).done); _iteratorNormalCompletion5 = true) {
+      var modalOpenButtonNode = _step5.value;
+
+      new ModalComponent(modalOpenButtonNode);
+    }
+  } catch (err) {
+    _didIteratorError5 = true;
+    _iteratorError5 = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion5 && _iterator5.return) {
+        _iterator5.return();
+      }
+    } finally {
+      if (_didIteratorError5) {
+        throw _iteratorError5;
       }
     }
   }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@
 
   const Utils = {
     /**
-     * Remove
+     * Remove a DOM element selected via itself or an event (target)
+     *
      * @param  {HTMLElement|Event} nodeOrEvent [description]
      * @return {void}
      */
@@ -27,43 +28,16 @@
       const alertNode = event.currentTarget.parentNode
 
       if (alertNode.classList.contains('fade')) {
-        alertNode.addEventListener('transitionend', Utils.remove, false);
+        alertNode.addEventListener('transitionend', Utils.remove, false)
       } else {
-        Utils.remove(alertNode);
+        Utils.remove(alertNode)
       }
 
       alertNode.classList.remove('in')
     }
   }
 
-  class DropDownMenuComponent {
-    constructor(node) {
-      node.addEventListener('click', this.open)
-      node.addEventListener('blur', this.close)
-    }
-
-    open(event) {
-      event.preventDefault()
-
-      event.currentTarget.parentElement.classList.toggle('open')
-    }
-
-    close(event) {
-      event.preventDefault()
-
-      event.currentTarget.parentElement.classList.remove('open')
-
-      // Trigger the click event on the target if it not opening another menu
-      if (event.relatedTarget && event.relatedTarget.getAttribute('data-toggle') !== 'dropdown') {
-        event.relatedTarget.click()
-      }
-    }
-  }
-
   class CollapsableBehavior {
-    // private _originNode
-    // private _targetNode
-
     constructor(node) {
       this._originNode = node
       const targetSelector = node.dataset.target ? node.dataset.target : node.hash
@@ -88,7 +62,7 @@
       // Force re-paint
       // @see http://stackoverflow.com/a/3485654/2736233
       node.style.height = currentHeight
-      this._targetNode.offsetHeight; // jshint ignore:line
+      this._targetNode.offsetHeight // jshint ignore:line
 
       node.style.height = maxHeight
     }
@@ -97,29 +71,29 @@
       event.preventDefault()
 
       if (this._targetNode.classList.contains('in')) {
-        this.hide();
+        this.hide()
       } else {
-        this.show();
+        this.show()
       }
     }
 
     show() {
       this._targetNode.addEventListener('transitionend', () => {
-        this.complete(false);
-      });
+        this.complete(false)
+      })
 
       this._originNode.setAttribute('aria-expanded', true)
       this._targetNode.classList.remove('collapse')
       this._targetNode.classList.add('collapsing')
       this._targetNode.style.height = '1px'
       this._setMaxHeight(this._targetNode)
-      this._targetNode.setAttribute('aria-expanded', true);
+      this._targetNode.setAttribute('aria-expanded', true)
     }
 
     hide() {
       this._targetNode.addEventListener('transitionend', () => {
-        this.complete(true);
-      }, false);
+        this.complete(true)
+      }, false)
 
       this._originNode.setAttribute('aria-expanded', false)
       this._originNode.classList.remove('collapse')
@@ -127,38 +101,113 @@
 
       // Force re-paint
       // @see http://stackoverflow.com/a/3485654/2736233
-      this._targetNode.style.height = getComputedStyle(this._targetNode).height;
-      this._targetNode.offsetHeight; // jshint ignore:line
+      this._targetNode.style.height = getComputedStyle(this._targetNode).height
+      this._targetNode.offsetHeight // jshint ignore:line
 
       this._targetNode.style.height = '1px'
-      this._targetNode.setAttribute('aria-expanded', false);
+      this._targetNode.setAttribute('aria-expanded', false)
     }
 
     complete(isHiding) {
-      this._targetNode.classList.remove('collapsing');
-      this._targetNode.classList.add('collapse');
-      this._targetNode.style.height = 'auto';
+      this._targetNode.classList.remove('collapsing')
+      this._targetNode.classList.add('collapse')
+      this._targetNode.style.height = 'auto'
 
       if (!isHiding) {
-        this._targetNode.classList.add('in');
+        this._targetNode.classList.add('in')
       } else {
-        this._targetNode.classList.remove('in');
+        this._targetNode.classList.remove('in')
       }
     }
   }
 
-  const collapsableNodes = document.querySelectorAll('[data-toggle=collapse]')
-  for (let collapsableNode of collapsableNodes) {
-    new CollapsableBehavior(collapsableNode);
+  class DropDownMenuComponent {
+    constructor(node) {
+      node.addEventListener('click', this.open)
+      node.addEventListener('blur', this.close)
+    }
+
+    open(event) {
+      event.preventDefault()
+      event.currentTarget.parentElement.classList.toggle('open')
+    }
+
+    close(event) {
+      event.preventDefault()
+      event.currentTarget.parentElement.classList.remove('open')
+
+      // Trigger the click event on the target if it not opening another menu
+      if (event.relatedTarget && event.relatedTarget.getAttribute('data-toggle') !== 'dropdown') {
+        event.relatedTarget.click()
+      }
+    }
   }
 
-  const alertNodes = document.querySelectorAll('[data-dismiss=alert]')
+  class ModalComponent {
+    constructor(modalOpenButtonNode) {
+      this._isFirstTime = true
+      this._bodyNode = document.querySelector('body')
+      this._modalWindowNode = document.querySelector(modalOpenButtonNode.dataset.target)
+
+      modalOpenButtonNode.addEventListener('click', this.open.bind(this))
+    }
+
+    open(event) {
+      event.preventDefault()
+
+      // We create and insert the background
+      this._modalBackdropNode = document.createElement('div')
+      this._modalBackdropNode.classList.add('modal-backdrop', 'fade', 'in')
+      this._bodyNode.appendChild(this._modalBackdropNode)
+
+      // We prepare the body
+      this._bodyNode.classList.add('modal-open')
+      this._bodyNode.style.paddingRight = '17px'
+
+      // We show the modal
+      this._modalWindowNode.classList.add('in')
+      this._modalWindowNode.style.display = 'block'
+
+      if (this._isFirstTime) {
+        this._isFirstTime = false
+
+        // We attach the close event to the dismiss buttons
+        const modalCloseButtonNodes = document.querySelectorAll('[data-dismiss="modal"]')
+        for (let modalCloseButtonNode of modalCloseButtonNodes) {
+          modalCloseButtonNode.addEventListener('click', this.close.bind(this))
+        }
+      }
+    }
+
+    close(event) {
+      event.preventDefault()
+
+      this._bodyNode.classList.remove('modal-open')
+      this._bodyNode.style.paddingRight = null
+      this._modalWindowNode.classList.remove('in')
+      this._modalWindowNode.style.display = 'none'
+
+      Utils.remove(this._modalBackdropNode)
+    }
+  }
+
+  const alertNodes = document.querySelectorAll('[data-dismiss="alert"]')
   for (let alertNode of alertNodes) {
-    new DismissableAlertComponent(alertNode);
+    new DismissableAlertComponent(alertNode)
   }
 
-  const dropDownNodes = document.querySelectorAll('[data-toggle=dropdown]')
-  for (let dropDownNode of dropDownNodes) {
-    new DropDownMenuComponent(dropDownNode);
+  const collapsableNodes = document.querySelectorAll('[data-toggle="collapse"]')
+  for (let collapsableNode of collapsableNodes) {
+    new CollapsableBehavior(collapsableNode)
   }
-})(window, window.document);
+
+  const dropDownNodes = document.querySelectorAll('[data-toggle="dropdown"]')
+  for (let dropDownNode of dropDownNodes) {
+    new DropDownMenuComponent(dropDownNode)
+  }
+
+  const modalOpenButtonNodes = document.querySelectorAll('[data-toggle="modal"]')
+  for (let modalOpenButtonNode of modalOpenButtonNodes) {
+    new ModalComponent(modalOpenButtonNode)
+  }
+})(window, window.document)
